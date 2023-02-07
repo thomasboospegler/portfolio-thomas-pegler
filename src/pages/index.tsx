f8d7fab7021e2e6f71e33abc269021b327ffa032
Thomas Boos Pegler <tboospegler@gmail.com>
2023-02-07 11:43:42 -0400

    chore: config tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,4 @@
 import Head from 'next/head'
-import { Inter } from '@next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -12,7 +9,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className=' bg-blue-300'>
+        <h1 className=" text-blue-700">
+          Hello world!
+        </h1>
       </main>
     </>
   )
